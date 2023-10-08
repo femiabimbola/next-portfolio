27c7a1fc057088c341ab375bec47ed5862d26fb4
femi <femiabimbola6@gmail.com>
2023-10-08 20:01:12 +0100

    added framer motion

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/Header'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
         <div className='bg-[#fbe2e3] -z-10 absolute top-[-6rem] right-[11rem] w-[31.25rem] h-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#dbd7fb] -z-10 absolute top-[-1rem] left-[35rem] w-[31.25rem] h-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        <Header />
         {children}
       </body>
     </html>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,21 @@
+"use client"
+
+import { motion } from 'framer-motion'
+
 const Header = () => {
   /**
    * 1 - The relative class makes z index works better
    */
   return (
     <header className="z-[999] relative">
-      <div className="fixed top-0 w-full left-1/2 h-[4.5rem] rounded-none border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.3] backdrop-blur-[0.5rem] sm:h-[3.25] sm:w-[36rem] sm:rounded-full">
+      <motion.div className="header-container" initial={{ y: -100, x: '-50%', opacity: 0 }}
+        animate={{ y: 0, x: '-50%', opacity: 1 }}
+      >
+      </motion.div>
+      {/* It should have been inside the nav, but becuase framer is used, it is not */}
+      <nav className='fixed top-[0.15rem] left-1/2 h-12'>
 
-      </div>
+      </nav>
     </header>
   )
 }
