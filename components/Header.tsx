360b65672c83c637aa28774262c4e5fd825c5e66
femi <femiabimbola6@gmail.com>
2023-10-08 19:11:14 +0100

    working on header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,14 @@
 const Header = () => {
-  <div> The Header one</div>
+  /**
+   * 1 - The relative class makes z index works better
+   */
+  return (
+    <header className="z-[999] relative">
+      <div className="fixed top-0 w-full left-1/2 h-[4.5rem] rounded-none border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.3] backdrop-blur-[0.5rem] sm:h-[3.25] sm:w-[36rem] sm:rounded-full">
+
+      </div>
+    </header>
+  )
 }
 
 export default Header
