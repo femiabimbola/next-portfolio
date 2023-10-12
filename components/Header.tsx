bfb04d695dcab73e6bc629cbe8e12e612ce3cbf2
Femi Abimbola <femiabimbola6@gmail.com>
2023-10-11 23:14:03 -0700

    started the intro

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,14 +15,17 @@ const Header = () => {
       >
       </motion.div>
       {/* It should have been inside the nav, but becuase framer is used, it is not */}
-      <nav className='fixed top-[0.15rem] left-1/2 h-12  -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
+      <nav className='flex fixed top-[0.15rem] left-1/2 h-12  -translate-x-1/2 p y-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0'>
         {/* Mine was gap-x-6 mt-3 */}
-        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium'>
+        <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:flex-nowrap sm:gap-5'>
           {
             links.map(link => (
-            <li key={link.hash} className='flex'>
-              <Link href={link.hash}> {link.name}</Link> 
-            </li>)
+            <motion.li key={link.hash} className='h-3/4 flex items-center justify-center'
+            initial={{y:-100, opacity:0}}
+            animate={{ y:0, opacity: 1}}
+            >
+              <Link href={link.hash} className='flex w-full items-center justify-center px-3 py-3 hover:bg-gray-950 transition'> {link.name}</Link> 
+            </motion.li>)
             )
           }
         </ul>
---
+++ components/intro.tsx
@@ -0,0 +1,9 @@
+const Intro = () => {
+  return (
+    <div>
+      This is the intro
+    </div>
+  )
+}
+
+export default Intro;
