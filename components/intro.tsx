9fe21028e6893b3ea5e7fc0c98f0062eb08d4877
Femi Abimbola <femiabimbola6@gmail.com>
2023-11-11 10:23:45 -0800

    working on the hero

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,10 +2,11 @@
 
 import Image from "next/image";
 import {motion} from "framer-motion";
+import Link from "next/link";
 
 const Intro = () => {
   return (
-    <section className="pt-28 md:pt-40">
+    <section className="pt-28 md:pt-40 max-w-[52rem] mb-28 ">
       <div className="flex items-center justify-center">
         <div className="absolute">
           <motion.div
@@ -37,17 +38,24 @@ const Intro = () => {
           </motion.span>
         </div>
       </div>
-      <p className="pt-20 md:pt-20">
+      <motion.p
+        className="pt-20 md:pt-20 mb-10 mt-4  text-2xl font-medium leading-[1.5] text-center"
+        initial={{opacity: 0, y: 200}}
+        animate={{opacity: 1, y: 0}}
+        transition={{type: "tween", duration: 0.3}}
+      >
         <span className="font-bold"> Hi, I'm Femi.</span> I'm a
         <span className="font-bold"> full-stack developer </span>
-        with over 4 years experience. I love building solutions that helps
-        individuals and companies. I focus on Javascript technologies ranging
-        from
-        <span className="font-bold">
-          {" "}
-          ReactJs, NextJs, NodeJs, and, NuxtJs.
+        with over 4 years experience. I love{" "}
+        <span className="italic">
+          building solutions that helps individuals and companies
         </span>
-      </p>
+        . I focus on Javascript technologies ranging from{" "}
+        <span className="font-bold">ReactJs, NextJs, NodeJs etc.</span>
+      </motion.p>
+      <div>
+        <Link href={"#contact"}> Contact me here</Link>
+      </div>
     </section>
   );
 };
