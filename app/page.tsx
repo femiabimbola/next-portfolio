710e0fd1699fabf4bc7b9f9226d6a2d3b9e6b0c7
Femi Abimbola <femiabimbola6@gmail.com>
2023-11-13 11:45:35 -0800

    finished the section divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/components/intro";
+import Divider from "@/components/section-divider";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4 overflow-hidden ">
       <Intro />
+      <Divider />
     </main>
   );
 }
--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import {motion} from "framer-motion";
+
 const Divider = () => {
   return (
-    <div>
-      <p> The section divider</p>
-    </div>
+    <motion.div
+      className="bg-gray-300 my-24 h-16 w-0.5 rounded-full hidden sm:block"
+      initial={{opacity: 0, y: 100}}
+      animate={{opacity: 1, y: 0}}
+      transition={{delay: 0.125}}
+    ></motion.div>
   );
 };
 
