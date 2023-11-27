105bd5d5362a04c173e400dc52097f1591ca9b4c
Femi Abimbola <femiabimbola6@gmail.com>
2023-11-27 14:50:24 -0800

    working on the experience

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,9 +1,26 @@
+import React from "react";
 import SectionHeading from "@/components/section-heading";
+import {experiencesData} from "@/lib/data";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
 const Experince = () => {
   return (
     <section>
       <SectionHeading> My Experience</SectionHeading>
+      <VerticalTimeline>
+        {/* Dont use key on third party component */}
+        {experiencesData.map((experienceItem, index) => (
+          <React.Fragment key={index}>
+            <VerticalTimelineElement>
+              <h3>{experienceItem.title}</h3>
+            </VerticalTimelineElement>
+          </React.Fragment>
+        ))}
+      </VerticalTimeline>
     </section>
   );
 };
