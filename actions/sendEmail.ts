af383d1dbf4376011d58093e3f49720904551419
Femi Abimbola <femiabimbola6@gmail.com>
2023-12-01 22:10:20 -0800

    working with resend

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,9 +1,20 @@
 "use server";
 
+import {Resend} from 'resend'
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
 export const sendEmail = async (formData: FormData) => {
 
   console.log("running on server");
   console.log(formData.get("senderEmail"));
   console.log(formData.get("message"));
+
+  resend.emails.send({
+    from: 'onboarding@resend.dev',
+    to: 'bytegrad@gmail.com',
+    subject:'Message from contact form',
+    text:'Hello World'
+  })
 };
 
