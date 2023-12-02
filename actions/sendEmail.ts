04158c201046674a929ca35ee4e4a362e19fb0fc
Femi Abimbola <femiabimbola6@gmail.com>
2023-12-02 06:51:56 -0800

    working on sending mail

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,20 +1,43 @@
 "use server";
 
+import { validateString } from '@/lib/utlis';
 import {Resend} from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
+
+
 export const sendEmail = async (formData: FormData) => {
 
   console.log("running on server");
-  console.log(formData.get("senderEmail"));
   console.log(formData.get("message"));
+  const message = formData.get('message')
+  const senderEmail =  formData.get('email')
+  
+  // learningPoint -> this is for text value comment  one to see what happens
+  
+  if(!message || typeof message !== 'string') {
+    return { status: 400, message: 'no message found'}
+  }
 
-  resend.emails.send({
-    from: 'onboarding@resend.dev',
-    to: 'bytegrad@gmail.com',
+  // if(typeof senderEmail !== 'string'){
+  //   return { error: 'invalid sender email'}
+  // }
+
+  if(!validateString(senderEmail, 500)) return { error: 'invalid sender email'}
+
+try {
+  await resend.emails.send({
+    from:"onboarding@resend.dev",
+    to: 'abimbola_mj@yahoo.com',
     subject:'Message from contact form',
-    text:'Hello World'
-  })
+    reply_to: senderEmail as string,
+    text:message 
+  })  
+} catch (error: any) {
+  console.log(error.message)
+  return{ error: error.message}
+}
+
 };
 
