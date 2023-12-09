2a219069f5de92a7a206990722afb65720ac8628
Femi Abimbola <femiabimbola6@gmail.com>
2023-12-09 05:51:32 -0800

    adding dark mode to header

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,10 +20,10 @@ export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 `}
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[#fbe2e3] -z-10 absolute top-[-6rem] right-[11rem] w-[31.25rem] h-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] -z-10 absolute top-[-1rem] left-[35rem] w-[31.25rem] h-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div className="bg-[#fbe2e3] -z-10 absolute top-[-6rem] right-[11rem] w-[31.25rem] h-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
+        <div className="bg-[#dbd7fb] -z-10 absolute top-[-1rem] left-[35rem] w-[31.25rem] h-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
         <ActiveSectionContextProvider>
           <Header />
           <Toaster position="top-center" />
--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,19 +1,47 @@
-import {useState} from "react";
-import {BsSun} from "react-icons/bs";
+"use client";
+
+import {useEffect, useState} from "react";
+import {BsMoon, BsSun} from "react-icons/bs";
 
 type Theme = "light" | "dark";
 
 const ThemeSwitch = () => {
   const [theme, setTheme] = useState<"light" | "dark">("light");
 
-  const toggleTheme = () => {};
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  // useEffect is when you need an external value on load
+  // When you need to call a function on load
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme") as Theme | null;
+    if (localTheme) {
+      setTheme(localTheme);
+      if (localTheme === "dark") {
+        document.documentElement.classList.add("dark");
+      }
+    } else if (window.matchMedia("(prefers-color-scheme:dark)").matches) {
+      setTheme("dark");
+      // The did not show
+    }
+  }, []);
 
   return (
     <button
-      className="fixed top-10 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active: scale-105 transition-all"
+      className="fixed top-10 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active: scale-105 transition-all dark:bg-gray-950"
       onClick={toggleTheme}
     >
-      <BsSun />
+      {theme === "light" ? <BsSun /> : <BsMoon />}
     </button>
   );
 };
